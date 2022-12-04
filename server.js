bb129459183d6a9caaf167288e43e21c9e91b269
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-12-03 17:16:13 -0700

    added packages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 // Import and require mysql2
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const consoleTable = require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
